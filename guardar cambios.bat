16bd5b2772f590615c12fb7f6a6f62ee48644a02
Farudelcazafemboys <farudcoronadobarreto@gmail.com>
2024-05-06 21:40:30 -0500

    Cambios guardados

--- guardar cambios.bat
+++ guardar cambios.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "C:\Users\trome\OneDrive\Documentos\GitHub\MiProyectoGodot"  REM
+cd /d "C:\Users\PERSONAL\Documents\GitHub\MiProyectoGodot"  REM
 git add .
 git commit -m "Cambios guardados"
 git push origin main
